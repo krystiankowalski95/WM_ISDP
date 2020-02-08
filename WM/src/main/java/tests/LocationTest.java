62d9f6be40ed6d0b45e5406892880cc1abf83e71
Szymon Gruda <szymon_gruda@o2.pl>
2020-02-08 22:05:19 +0100

    test

--- WM/src/main/java/tests/LocationTest.java
+++ WM/src/main/java/tests/LocationTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -27,6 +28,37 @@ public class LocationTest {
             WebElement iresDiv = driver.findElement(By.id("ires"));
             iresDiv.findElements(By.tagName("a")).get(0).click();
             System.out.println(driver.getPageSource());
+
+            driver.get("http://localhost:8080/faces/main/index.xhtml");
+            driver.findElement(By.linkText("Logowanie")).click();
+            driver.findElement(By.name("j_username")).click();
+            driver.findElement(By.name("j_username")).click();
+            driver.findElement(By.name("j_username")).clear();
+            driver.findElement(By.name("j_username")).sendKeys("JDoe");
+            driver.findElement(By.name("j_password")).click();
+            driver.findElement(By.name("j_password")).clear();
+            driver.findElement(By.name("j_password")).sendKeys("P@ssw0rd");
+            driver.findElement(By.xpath("//input[@value='Zaloguj']")).click();
+            driver.findElement(By.linkText("Lokalizacja")).click();
+            driver.findElement(By.linkText("Utwórz nową lokalizację")).click();
+            driver.findElement(By.id("CreateLocationForm:locationSymbol")).click();
+            driver.findElement(By.id("CreateLocationForm:locationSymbol")).clear();
+            driver.findElement(By.id("CreateLocationForm:locationSymbol")).sendKeys("TE-00-00-00");
+            driver.findElement(By.id("CreateLocationForm:locationType")).click();
+            new Select(driver.findElement(By.id("CreateLocationForm:locationType"))).selectByVisibleText("CAŁA");
+            driver.findElement(By.xpath("//option[@value='SHELF1']")).click();
+            driver.findElement(By.name("CreateLocationForm:j_idt35")).click();
+            driver.findElement(By.linkText("Lokalizacja")).click();
+            driver.findElement(By.linkText("Lista lokalizacji")).click();
+            driver.findElement(By.name("j_idt27:j_idt28:10:onlyWarehouse:j_idt38")).click();
+            driver.findElement(By.id("EditLocationForm:locationType")).click();
+            new Select(driver.findElement(By.id("EditLocationForm:locationType"))).selectByVisibleText("POŁOWA");
+            driver.findElement(By.xpath("//option[@value='SHELF2']")).click();
+            driver.findElement(By.name("EditLocationForm:j_idt32")).click();
+            driver.findElement(By.name("j_idt27:j_idt28:10:onlyWarehouse:j_idt39")).click();
+            driver.findElement(By.name("DeleteLocationForm:j_idt31")).click();
+            driver.findElement(By.linkText("Wylogowanie")).click();
+            driver.findElement(By.name("j_idt27:j_idt31")).click();
         } finally {
             driver.quit();
         }
