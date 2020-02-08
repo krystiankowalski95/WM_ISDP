848862cecddc03cf3a4682a19b8cf5cdd935ee80
Szymon Gruda <szymon_gruda@o2.pl>
2020-02-08 21:58:35 +0100

    o

--- WM/src/main/java/tests/LocationTest.java
+++ WM/src/main/java/tests/LocationTest.java
@@ -1,36 +1,68 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class LocationTest {
-    private WebDriver driver;
-    @Test
-    public void testEasy() {
-        driver.get("http://demo.guru99.com/test/guru99home/");
-        String title = driver.getTitle();
-        Assert.assertTrue(title.contains("Demo Guru99 Page"));
-    }
-    @BeforeTest
-    public void beforeTest() {
+    public static void main(String [] args) {
         FirefoxBinary firefoxBinary = new FirefoxBinary();
         firefoxBinary.addCommandLineOptions("--headless");
         System.setProperty("webdriver.gecko.driver", "/var/lib/jenkins/workspace/test/WM/geckodriver/geckodriver");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setBinary(firefoxBinary);
         FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
-        driver = new FirefoxDriver();
+        try {
+            driver.get("http://www.google.com");
+            driver.manage().timeouts().implicitlyWait(4,
+                    TimeUnit.SECONDS);
+            WebElement queryBox = driver.findElement(By.name("q"));
+            queryBox.sendKeys("headless firefox");
+            WebElement searchBtn = driver.findElement(By.name("btnK"));
+            searchBtn.click();
+            WebElement iresDiv = driver.findElement(By.id("ires"));
+            iresDiv.findElements(By.tagName("a")).get(0).click();
+            System.out.println(driver.getPageSource());
+        } finally {
+            driver.quit();
+        }
     }
-    @AfterTest
-    public void afterTest() {
-        driver.quit();
-    }
+}
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxBinary;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxOptions;
+//import org.testng.Assert;
+//import org.testng.annotations.AfterTest;
+//import org.testng.annotations.BeforeTest;
+//import org.testng.annotations.Test;
+//
+//public class LocationTest {
+//    private WebDriver driver;
+//    @Test
+//    public void testEasy() {
+//        driver.get("http://demo.guru99.com/test/guru99home/");
+//        String title = driver.getTitle();
+//        Assert.assertTrue(title.contains("Demo Guru99 Page"));
+//    }
+//    @BeforeTest
+//    public void beforeTest() {
+//        FirefoxBinary firefoxBinary = new FirefoxBinary();
+//        firefoxBinary.addCommandLineOptions("--headless");
+//        System.setProperty("webdriver.gecko.driver", "/var/lib/jenkins/workspace/test/WM/geckodriver/geckodriver");
+//        FirefoxOptions firefoxOptions = new FirefoxOptions();
+//        firefoxOptions.setBinary(firefoxBinary);
+//        FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
+//        driver = new FirefoxDriver();
+//    }
+//    @AfterTest
+//    public void afterTest() {
+//        driver.quit();
+//    }
     /*private WebDriver driver;
     private String baseUrl;
     private boolean acceptNextAlert = true;
