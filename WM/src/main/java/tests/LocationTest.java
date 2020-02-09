98b11c0b2499dee622ba593f072e2b607c9f6887
Szymon Gruda <szymon_gruda@o2.pl>
2020-02-09 10:56:22 +0100

    e

--- WM/src/main/java/tests/LocationTest.java
+++ WM/src/main/java/tests/LocationTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,9 +11,11 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
+import static org.testng.FileAssert.fail;
 
 public class LocationTest {
-
+    private StringBuffer verificationErrors = new StringBuffer();
+    private FirefoxDriver driver;
     @Test
     public void LocationCase() {
         FirefoxBinary firefoxBinary = new FirefoxBinary();
@@ -20,25 +23,28 @@ public class LocationTest {
         System.setProperty("webdriver.gecko.driver", "/var/lib/jenkins/workspace/test/WM/geckodriver/geckodriver");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setBinary(firefoxBinary);
-        FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
+        driver = new FirefoxDriver(firefoxOptions);
         try {
             driver.get("http://localhost:8080/faces/main/index.xhtml");
             driver.findElement(By.linkText("Sign in")).click();
-            driver.findElement(By.name("j_username")).click();
-            driver.findElement(By.name("j_username")).click();
             driver.findElement(By.name("j_username")).click();
             driver.findElement(By.name("j_username")).clear();
             driver.findElement(By.name("j_username")).sendKeys("JDoe");
             driver.findElement(By.name("j_password")).click();
             driver.findElement(By.name("j_password")).clear();
             driver.findElement(By.name("j_password")).sendKeys("P@ssw0rd");
-            driver.findElement(By.name("j_username")).click();
             driver.findElement(By.xpath("//input[@value='Sign in']")).click();
-            driver.findElement(By.xpath("//h4")).click();
             try {
-                assertEquals("Authenticated user: JDoe", driver.findElement(By.xpath("//h4")).getText());
+                assertEquals(driver.findElement(By.xpath("//h4")).getText(), "Authenticated user: JDoe");
             } catch (Error e) {
-                System.out.println(e.toString());
+                verificationErrors.append(e.toString());
+            }
+            driver.findElement(By.linkText("Location")).click();
+            driver.findElement(By.linkText("Locations list")).click();
+            try {
+                assertEquals(driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[10]/td")).getText(), "AA-01-04-04");
+            } catch (Error e) {
+                verificationErrors.append(e.toString());
             }
             driver.findElement(By.linkText("Location")).click();
             driver.findElement(By.linkText("Create new location")).click();
@@ -52,14 +58,14 @@ public class LocationTest {
             driver.findElement(By.linkText("Location")).click();
             driver.findElement(By.linkText("Locations list")).click();
             try {
-                assertEquals("TT-00-00-00", driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[11]/td")).getText());
+                assertEquals(driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[11]/td")).getText(), "TT-00-00-00");
             } catch (Error e) {
-                System.out.println(e.toString());
+                verificationErrors.append(e.toString());
             }
             try {
-                assertEquals("WHOLE", driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[11]/td[2]")).getText());
+                assertEquals(driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[11]/td[2]")).getText(), "WHOLE");
             } catch (Error e) {
-                System.out.println(e.toString());
+                verificationErrors.append(e.toString());
             }
             driver.findElement(By.name("j_idt27:j_idt28:10:onlyWarehouse:j_idt38")).click();
             driver.findElement(By.id("EditLocationForm:locationType")).click();
@@ -67,26 +73,26 @@ public class LocationTest {
             driver.findElement(By.xpath("//option[@value='SHELF2']")).click();
             driver.findElement(By.name("EditLocationForm:j_idt32")).click();
             try {
-                assertEquals("HALF", driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[11]/td[2]")).getText());
+                assertEquals(driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[11]/td")).getText(), "TT-00-00-00");
             } catch (Error e) {
-                System.out.println(e.toString());
+                verificationErrors.append(e.toString());
             }
             try {
-                assertEquals("TT-00-00-00", driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[11]/td")).getText());
+                assertEquals(driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[11]/td[2]")).getText(), "HALF");
             } catch (Error e) {
-                System.out.println(e.toString());
+                verificationErrors.append(e.toString());
             }
             driver.findElement(By.name("j_idt27:j_idt28:10:onlyWarehouse:j_idt39")).click();
             driver.findElement(By.name("DeleteLocationForm:j_idt31")).click();
             try {
-                assertEquals("AA-01-04-04", driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[10]/td")).getText());
+                assertEquals(driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[10]/td")).getText(), "AA-01-04-04");
             } catch (Error e) {
-                System.out.println(e.toString());
+                verificationErrors.append(e.toString());
             }
             try {
-                assertEquals("QUARTER", driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[10]/td[2]")).getText());
+                assertEquals(driver.findElement(By.xpath("//form[@id='j_idt27']/table/tbody/tr[10]/td[2]")).getText(), "QUARTER");
             } catch (Error e) {
-                System.out.println(e.toString());
+                verificationErrors.append(e.toString());
             }
             driver.findElement(By.linkText("Sign out")).click();
             driver.findElement(By.name("j_idt27:j_idt31")).click();
@@ -94,6 +100,14 @@ public class LocationTest {
 
         } finally {
             driver.quit();
+        }
+    }
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
         }
     }
 }
